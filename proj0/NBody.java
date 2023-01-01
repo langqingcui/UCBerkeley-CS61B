222ef82e3b163039ac43eeaba6ee41e41d18424e
Langqing Cui <langqing.cui@outlook.com>
2023-01-02 01:03:05 +0800

    completed Planet.java and some parts of NBody.java

---
+++ proj0/NBody.java
@@ -0,0 +1,36 @@
+public class NBody {
+    public static double readRadius(String fileName) {
+        In in = new In(fileName);
+        int N = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int N = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planets = new Planet[N];
+        for (int i = 0; i < N; i++) {
+            double xp = in.readDouble();
+            double yp = in.readDouble();
+            double xv = in.readDouble();
+            double yv = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xp, yp, xv, yv, m, img);
+        }
+        return planets;
+    }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        
+        double radius = readRadius(filename);
+        Planet[] planets = readPlanets(filename);
+
+        
+    }
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,83 @@
+public class Planet {
+    public double xxPos, yyPos, xxVel, yyVel, mass;
+    public String imgFileName;
+
+    final static double G = 6.67e-11;
+
+    public Planet(double xp, double yp, double xV, double yV, double m, String img) {
+        xxPos = xp;
+        yyPos = yp;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        double r = Math.sqrt((this.xxPos - p.xxPos) * (this.xxPos - p.xxPos) 
+                    + (this.yyPos - p.yyPos) * (this.yyPos - p.yyPos));
+        return r;
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = calcDistance(p);
+        return G * this.mass * p.mass / (r * r);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double f = this.calcForceExertedBy(p);
+        double r = this.calcDistance(p);
+        double fx = (f * (p.xxPos - this.xxPos)) / r;
+        return fx;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double f = this.calcForceExertedBy(p);
+        double r = this.calcDistance(p);
+        double fy = (f * (p.yyPos - this.yyPos)) / r;
+        return fy;
+    }
+
+    public double calcNetForceExertedByX(Planet[] allPlanets) {
+        double netForceX = 0;
+        for (Planet planet : allPlanets) {
+            if (this.equals(planet)) {
+                continue;
+            } else {
+                netForceX += this.calcForceExertedByX(planet);
+            }
+        }
+        return netForceX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] allPlanets) {
+        double netForceY = 0;
+        for (Planet planet : allPlanets) {
+            if (this.equals(planet)) {
+                continue;
+            } else {
+                netForceY += this.calcForceExertedByY(planet);
+            }
+        }
+        return netForceY;
+    }
+
+    public void update(double dt, double fX, double fY) {
+        double aX = fX / this.mass;
+        double aY = fY / this.mass;
+        this.xxVel += dt * aX;
+        this.yyVel += dt * aY;
+        this.xxPos += dt * this.xxVel;
+        this.yyPos += dt * this.yyVel;
+    }
+
+}
